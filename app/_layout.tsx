49a817ad0ba088b1eb87818fd45892e887c11c5b
Jatesh Parikh <jateshparikh97@gmail.com>
2024-07-09 22:36:37 +0530

    final changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -27,13 +27,13 @@ export default function RootLayout() {
   return (
     <TimerProvider>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="meditate/[id]" options={{ headerShown: false }} />
-        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen
           name="(modal)/adjust-meditation-duration"
           options={{ headerShown: false, presentation: "modal" }}
         />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="meditate/[id]" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
       </Stack>
     </TimerProvider>
   );
